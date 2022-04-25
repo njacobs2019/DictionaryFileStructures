c546024539c7dd8fa473328255882808d9517d47
Nicholas <nicholas.jacobs@maine.edu>
2022-04-25 12:13:53 -0400

    Fixed compare.java

--- Reference/Compare.java
+++ Reference/Compare.java
@@ -1,29 +1,27 @@
 	static int compare(String a, String b){
-		 return 1 if ab
-		 return 0 if a=b
-		 return -1 if ab
+		 // return 1 if a>b
+		 // return 0 if a=b
+		 // return -1 if a<b
 
 		int len_a = a.length();
 		int len_b = b.length();
 
-		int min;
+		int min=len_b;
 		if(len_alen_b)
 			min=len_a;
-		else
-			min=len_b;
 
 		for(int i=0; imin; i++){
-			if(a.charAt(i)b.charAt(i)){
+			if(a.charAt(i)<b.charAt(i)){
 				return -1;
 			}
-			if(a.charAt(i)b.charAt(i)){
+			if(a.charAt(i)>b.charAt(i)){
 				return 1;
 			}
 		}
 
-		case where same length they are same, return 0
-		case where a is shorter, return -1
-		 case where b is shorter, return 1
+		// case where same length they are same, return 0
+		// case where a is shorter, return -1
+		// case where b is shorter, return 1
 
 		if(len_a==len_b)
 			return 0;
