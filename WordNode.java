00cdf0418fa4e2fad6cc0bab8862eca025f01b5a
Nicholas <nicholas.jacobs@maine.edu>
2022-04-25 11:39:18 -0400

    Created WordNode class

---
+++ WordNode.java
@@ -0,0 +1,22 @@
+public class WordNode({
+	private String name;
+	private String pos;
+	private String definition;
+	WordNode next;
+
+	public WordNode(String inName, String inPos, String inDefn){
+		this.name = inName;
+		this.pos = inPos;
+		this.definition = inDef;
+		this.next = null;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	// other get methods
+
+	// toString method
+
+}
