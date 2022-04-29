b9b19fd032e3b75d0e1adbec2725a3c7286d4fb1
SunGodRa1 <naterob315@gmail.com>
2022-04-29 10:34:24 -0400

    B+

--- BPlusTree.java
+++ BPlusTree.java
@@ -1,3 +1,22 @@
 public class BPlusTree.java{
-	
+	int max, i;
+	Bucket root = null;
+
+	public BPlusTree(int max){
+		System.out.println("B+ tree created with max size " + max);
+		this.max = max;
+	}
+
+	public void add(String inName, String inPos, String inDef){
+		WordNode n = new WordNode(inName, inPos, inDef);
+
+		if(root = null){
+			root = new Bucket(max, true);
+			root.BSize = 1;
+			root.word[0] = n;
+		}
+		else{
+			Bucket p = root;
+		}
+	}
 }
