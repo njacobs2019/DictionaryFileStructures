1b1199460173807a408e4ff6c60e9d8064c6e636
njacobs2019 <59352054+njacobs2019@users.noreply.github.com>
2022-05-04 16:12:45 -0400

    Created and tested HashList and HashNode classes

---
+++ HashList_Tester.java
@@ -0,0 +1,16 @@
+public class HashList_Tester{
+
+	public static void main(String[] args){
+		WordNode defn1 = new WordNode("Aback","adv.","Behind; in the rear.");
+		WordNode defn2 = new WordNode("Aback","adv.","Toward the back or rear; backward.");
+		WordNode defn3 = new WordNode("Abacus","n.","The uppermost member or division of the capital of a column, immediately under the architrave. See Column.");
+
+		HashList hl = new HashList();
+		hl.add(defn1);
+		hl.add(defn2);
+		hl.add(defn3);
+
+		System.out.format("This hashlist contains %s words\n",hl.length());
+		System.out.println(hl);
+	}
+}
---
+++ HashNode.java
@@ -0,0 +1,11 @@
+// This is a node in the hashtable's linked lists
+// This node holds a single wordlist i.e. a single word and all of its definitions
+
+class HashNode extends WordList{
+	public HashNode next;
+
+	public HashNode(String inName){
+		super(inName);
+		next = null;
+	}
+}
