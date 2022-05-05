362a81142b2c23fb73efda0e25b06584695a1eb0
njacobs2019 <59352054+njacobs2019@users.noreply.github.com>
2022-05-05 10:51:12 -0400

    Wrote function to test printAll

--- BPlusTree_Tester.java
+++ BPlusTree_Tester.java
@@ -5,9 +5,42 @@ import java.io.FileNotFoundException;
 public class BPlusTree_Tester{
 
 	public static void main(String[] args){
-		addTester();
+		printAllTester();
 	}
 
+	public static void printAllTester(){
+		//Initializes file reader
+		File f = new File("Dataset.tsv");
+
+		BPlusTree tree = new BPlusTree(3);
+		WordNode n;
+
+		try{
+			Scanner scan = new Scanner(f);
+			String line;
+			line = scan.nextLine();  // Reads in the first line of column labels
+			
+			// while(scan.hasNext()){
+			for(int i=0; i<20; i++){
+				line = scan.nextLine();
+				n = new WordNode(line);
+				tree.add(n);
+			}
+
+			System.out.println("BFS of tree:");
+			tree.printBFS();
+
+			System.out.println("\nPrint all leaves:");
+			tree.printAll();
+			System.out.println();
+			scan.close();
+		}
+		catch(FileNotFoundException e){
+			System.out.format("The file was not found.\n");
+		}
+	}
+
+	// This is what we were running 5/4 at night to print out a lot of stuff
 	public static void addTester(){
 		//Initializes file reader
 		File f = new File("Dataset.tsv");
@@ -25,8 +58,8 @@ public class BPlusTree_Tester{
 				line = scan.nextLine();
 				System.out.print("BFS:  ");
 				tree.printBFS();
-				System.out.println();				
 				n = new WordNode(line);
+				System.out.format("\nAdding %s\n",n.getName());				
 				System.out.format("i %d\n",i);
 				tree.add(n);
 			}
